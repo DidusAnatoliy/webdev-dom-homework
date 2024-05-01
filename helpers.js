21b51117af6ae39efc527deacec4097c3d473c40
bereg.007@mail.ru <you@example.com>
2024-05-01 19:42:45 +0300

    finish

--- helpers.js
+++ helpers.js
@@ -1,9 +1,23 @@
-export function sanitizeHtml(str){
-return str. 
-replaceAll("&", "&amp;")
-.replaceAll("<", "&lt;")
-.replaceAll(">", "&gt;")
-.replaceAll('"', "&quot;")
-.replaceAll("%BEGIN_QUOTE", "<div class='quote'>")
-.replaceAll("END_QUOTE%", "</div>")
+import { format } from "date-fns"
+
+export function sanitize(text) {
+    return text
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;")
+        .replaceAll("%BEGIN_QUOTE", "<div class='quote'>")
+        .replaceAll("END_QUOTE%", "</div>")
+}
+
+export function normalizeComments(comments) {
+    return comments.map((comment) => {
+        return {
+            name: comment.author.name,
+            time: format(new Date(comment.date), "yyyy-MM-dd hh.mm.ss"),
+            comment: comment.text,
+            likes: comment.likes,
+            isLiked: false,
+        }
+    });
 }
